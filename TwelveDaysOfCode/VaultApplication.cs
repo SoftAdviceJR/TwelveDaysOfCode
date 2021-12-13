57c58c0650867cca673c1878a692fb9d8ee9db23
SoftAdviceJR <jeroen@softadvice.be>
2021-12-13 19:55:49 +0100

    Day 1

---
+++ TwelveDaysOfCode/VaultApplication.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Diagnostics;
+
+using MFiles.VAF;
+using MFiles.VAF.AppTasks;
+using MFiles.VAF.Common;
+using MFiles.VAF.Configuration;
+using MFiles.VAF.Extensions;
+
+using MFilesAPI;
+
+namespace TwelveDaysOfCode
+{
+	/// <summary>
+	/// The entry point for this Vault Application Framework application.
+	/// </summary>
+	/// <remarks>Examples and further information available on the developer portal: http://developer.m-files.com/. </remarks>
+	public class VaultApplication
+		: ConfigurableVaultApplicationBase<Configuration>
+	{
+	}
+}
